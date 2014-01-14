edd0e0baacbb4620a4a3cfee382efd3dcb4baa98
Andrés Cirugeda Esco <andres@aspgems.com>
2014-01-14 12:46:25 +0100

    Added method missing to avoid hashie in all attribute calls

--- lib/bitbucket_v2/models/base.rb
+++ lib/bitbucket_v2/models/base.rb
@@ -10,6 +10,15 @@ module BitbucketV2
         @hashie = ::Hashie::Mash.new(attributes)
       end
 
+      def method_missing(name, *args, &block)
+        super unless hashie.respond_to?(name)
+        hashie.send(name)
+      end
+
+      def respond_to_missing?(name, include_private = false)
+        hashie.respond_to?(name)
+      end
+
       class << self
         def make_request(url, options = {})
           BitbucketV2::Request.request(url, options)
